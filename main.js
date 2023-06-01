33424f7f17fab029ea02e52a41453f96092ead17
asdribelb <asdribelb@gmail.com>
2023-06-01 15:09:32 -0400

    Correcciones proyecto java

--- main.js
+++ main.js
@@ -1,117 +1,200 @@
-/*Funcion de los articulos */
+const productos = [
+    {
+        id: "iphone-7",
+        titulo: "Iphone 7",
+        imagen: "../img/iphone 7.jpg",
+        categoria: {
+            nombre: "Apple",
+            id: "apple",
+        },
+        precio: 150990,
+    },
+    {
+        id: "iphone-8",
+        titulo: "Iphone 8",
+        imagen: "../img/iphone 8.jpg",
+        categoria: {
+            nombre: "Apple",
+            id: "apple",
+        },
+        precio: 169990,
+    },
+    {
+        id: "iphone-x",
+        titulo: "iphone X",
+        imagen: "../img/iphone x.jpg",
+        categoria: {
+            nombre: "Apple",
+            id: "apple",
+        },
+        precio: 415990,
+    },
+    {
+        id: "iphone-11",
+        titulo: "iphone 11",
+        imagen: "../img/iphone 11.jpg",
+        categoria: {
+            nombre: "Apple",
+            id: "apple",
+        },
+        precio: 500990,
+    },
+    {
+        id: "iphone-12",
+        titulo: "iphone 12",
+        imagen: "../img/iphone 12.jpg",
+        categoria: {
+            nombre: "Apple",
+            id: "apple",
+        },
+        precio: 310990,
+    },
+    {
+        id: "iphone-13",
+        titulo: "iphone 13",
+        imagen: "../img/iphone 13.jpg",
+        categoria: {
+            nombre: "Apple",
+            id: "apple",
+        },
+        precio: 415990,
+    },
+    {
+        id: "samsung-s21",
+        titulo: "samsung s21",
+        imagen: "../img/samsung galaxy s21.jpg",
+        categoria: {
+            nombre: "Samsung",
+            id: "samsung",
+        },
+        precio: 250990,
+    },
+    {
+        id: "samsung-note20",
+        titulo: "samsung note20",
+        imagen: "../img/samsung20.jpg",
+        categoria: {
+            nombre: "Samsung",
+            id: "samsung",
+        },
+        precio: 310990,
+    },
+    {
+        id: "samsung-a52",
+        titulo: "samsung a52",
+        imagen: "../img/samsung A52.jpg",
+        categoria: {
+            nombre: "Samsung",
+            id: "samsung",
+        },
+        precio: 415990,
+    },
+    {
+        id: "samsung-a32",
+        titulo: "samsung a32",
+        imagen: "../img/samsung a32.jpg",
+        categoria: {
+            nombre: "Samsung",
+            id: "samsung",
+        },
+        precio: 400990,
+    },
+    {
+        id: "samsung-s22",
+        titulo: "samsung s22",
+        imagen: "../img/samsungs22.jpg",
+        categoria: {
+            nombre: "Samsung",
+            id: "samsung",
+        },
+        precio: 500990,
+    },
+    {
+        id: "samsung-s20",
+        titulo: "samsung s20",
+        imagen: "../img/samsungs20.jpg",
+        categoria: {
+            nombre: "Samsung",
+            id: "samsung",
+        },
+        precio: 699990,
+    },
+];
 
-let articulo = function (nombre, marca, precio, stock) {
-    this.nombre = nombre,
-        this.marca = marca,
-        this.precio = precio,
-        this.stock = stock
-}
+// Variables
+const carContenido = document.querySelector("#contenido");
+const verCarrito = document.querySelector("#verCarrito");
+const modalContainer = document.querySelector("#modal-container");
 
-let articulo1 = new articulo('Iphone 7', 'Apple', 150990, 10)
-let articulo2 = new articulo('Iphone 8', 'Apple', 200000, 10)
-let articulo3 = new articulo('Iphone X', 'Apple', 239990, 20)
-let articulo4 = new articulo('Iphone 11', 'Apple', 419990, 30)
-let articulo5 = new articulo('Iphone 12 PRO', 'Apple', 500000, 50)
-let articulo6 = new articulo('Iphone 13', 'Apple', 699990, 60)
+let carrito = [];
 
-let articulo7 = new articulo('Samsung Galaxy S21', 'Samsung', 250000, 15)
-let articulo8 = new articulo('Samsung Galaxy Note 20', 'Samsung', 310990, 20)
-let articulo9 = new articulo('Samsung Galaxy A52', 'Samsung', 415990, 25)
-let articulo10 = new articulo('Samsung Galaxy A32', 'Samsung', 499000, 30)
-let articulo11 = new articulo('Samsung Galaxy Watch 3', 'Samsung', 560000, 45)
-let articulo12 = new articulo('Samsung Galaxy Buds Pro', 'Samsung', 799990, 65)
+productos.forEach((product) => {
+    const content = document.createElement("div");
+    content.classList.add("card_celular");
+    content.classList.add("product");
+    content.innerHTML = `
+        <img src="${product.imagen}" alt="${product.titulo}">
+        <h3>${product.titulo}</h3>
+        <p class="celular_texto">${product.precio}</p>
+        <a href="#" class="boton" id="btn7">Agregar al Carrito</a>
+    `;
 
-let apple = [articulo1, articulo2, articulo3, articulo4, articulo5, articulo6]
-let samsung = [articulo7, articulo8, articulo9, articulo10, articulo11, articulo12]
+    const comprar = content.querySelector(".boton");
 
-console.table(apple)
-console.table(samsung)
+    comprar.addEventListener("click", () => {
+        carrito.push({
+            id: product.id,
+            titulo: product.titulo,
+            imagen: product.imagen,
+            categoria: product.categoria,
+            precio: product.precio,
+        });
+
+        console.log(carrito);
+    });
+
+    carContenido.append(content);
+});
+
+verCarrito.addEventListener("click", () => {
+    const modalHeader = document.createElement("div");
+    modalHeader.className = "modal-header";
+    modalHeader.innerHTML = `
+        <h1 class="modal-header-title"></h1>
+        <h1 class="modal-header-button">X</h1>
+    `;
+    modalContainer.append(verCarrito);
+
+    const modalbutton = document.createElement("h1")
+    modalbutton.innerText = "x";
+    modalbutton.className = "modal-header-button";
+
+    modalbutton.addEventListener("click", () =>{
+        modalContainer.style.display = "none";
+
+    });
+
+    carrito.forEach((product) => {
+        let carritoContent = document.createElement("div");
+        carritoContent.className = "modal-content";
+        carritoContent.innerHTML = `
+            <img src="${product.imagen}" alt="${product.titulo}">
+            <h3>${product.titulo}</h3>
+            <p class="celular_texto">$${product.precio}</p>
+        `;
+
+        modalContainer.append(carritoContent);
+    });
 
 
-/*Bienvenida a mi Tienda Celulares Bellorin*/
+    const total = carrito.reduce((acc, el) => acc + el.precio, 0);
+    const modalFooter = document.createElement("div");
+    modalFooter.className = "modal-footer";
+    modalFooter.innerHTML = `
+    <p class="modal-footer-text">Total a pagar: $${total}</p>
+    <a href="#" class="modal-footer-button">Comprar</a>
+    `;
+    modalContainer.append(modalFooter);
+    
 
-alert("Bienvenidos a mi Tienda de Celulares, trabajamos con las siguientes marcas: \nAPPLE \nSAMSUNG");
-
-let marcaIngresada;
-
-while (true) {
-    marcaIngresada = prompt("Ingrese la Marca que desea comprar");
-
-    if (marcaIngresada.toLowerCase() === "apple") {
-        console.table(apple);
-        break;
-    } else if (marcaIngresada.toLowerCase() === "samsung") {
-        console.table(samsung);
-        break;
-    } else {
-        alert("No tenemos esa marca disponible");
-    }
-}
-
-function calcularTotalCarrito(marcaIngresada) {
-    let carrito = [];
-    let total = 0;
-
-    if (marcaIngresada.toLowerCase() === "apple") {
-        while (true) {
-            let nombreArticulo = prompt("Ingrese el nombre del artículo que desea agregar al carrito (Escriba 'cancelar' para finalizar la compra)")
-            if (nombreArticulo.toLowerCase() === "cancelar") {
-                break;
-            }
-
-            let cantidadArticulo = parseInt(prompt("Ingrese la cantidad del artículo que desea agregar al carrito"));
-
-            // Buscar el artículo en la lista de Apple
-            let articuloEncontrado = apple.find((articulo) => articulo.nombre.toLowerCase() === nombreArticulo.toLowerCase());
-
-            if (articuloEncontrado) {
-                if (articuloEncontrado.stock >= cantidadArticulo) {
-                    articuloEncontrado.stock -= cantidadArticulo;
-                    articuloEncontrado.cantidad += cantidadArticulo;
-                    carrito.push(articuloEncontrado);
-                    articuloEncontrado.stock--;
-                    total += articuloEncontrado.precio * cantidadArticulo;
-                    alert("Artículo agregado al carrito");
-                } else {
-                    alert("No hay stock disponible de ese artículo");
-                }
-            } else {
-                alert("El artículo ingresado no existe en la lista de Apple");
-            }
-        }
-    } else if (marcaIngresada.toLowerCase() === "samsung") {
-        while (true) {
-            let nombreArticulo = prompt("Ingrese el nombre del artículo que desea agregar al carrito (Escriba 'cancelar' para finalizar la compra)");
-
-            if (nombreArticulo.toLowerCase() === "cancelar") {
-                break;
-            }
-
-            let cantidadArticulo = parseInt(prompt("Ingrese la cantidad del artículo que desea agregar al carrito"));
-
-            // Buscar el artículo en la lista de Samsung
-            let articuloEncontrado = samsung.find((articulo) => articulo.nombre.toLowerCase() === nombreArticulo.toLowerCase());
-
-            if (articuloEncontrado) {
-                if (articuloEncontrado.stock >= cantidadArticulo) {
-                    articuloEncontrado.stock -= cantidadArticulo;
-                    articuloEncontrado.cantidad += cantidadArticulo;
-                    carrito.push(articuloEncontrado);
-                    total += articuloEncontrado.precio * cantidadArticulo;
-                    alert("Artículo agregado al carrito");
-                } else {
-                    alert("No hay stock disponible de ese artículo");
-                }
-            } else {
-                alert("El artículo ingresado no existe en la lista de Samsung");
-            }
-        }
-    }
-
-    console.log("Artículos en el carrito:");
-    console.log(carrito);
-
-    alert("Suma total de los artículos en el carrito: $" + total);
-}
-
-calcularTotalCarrito(marcaIngresada);
+});
